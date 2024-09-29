4e750986a87d93c646df1c970f962b9a7c7df59b
Emelie Forselius <emelieforselius@outlook.com>
2024-09-29 22:36:32 +0200

    Styled LandingPage and added image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from "../styles/LandingPage.module.css"
 
 const LandingPage = () => {
   const links = [{ href: "/todo" }];
 
   return (
-    <div>
-      <h1>Task Tracker</h1>
-      <p>
+    <div className={styles.todoPageContainer}>
+      <div className={styles.textContainer}>
+      <h1 className={styles.title}>Task Tracker</h1>
+      <p className={styles.description}>
         With our todo list, you can easily add, remove and keep track of your
-        tasks. Capture your thoughts and plan your day!
+        tasks. <br /> Capture your thoughts and plan your day!
         <br />
         <br />
         Wanna get started? Click <Link href="/todo">here</Link> to add your
         tasks :)
       </p>
+      </div>
       <Image
         src="/website-launch-checklist.svg"
         alt="Website launch checklist"
         width={400}
         height={400}
+        className={styles.image}
       ></Image>
     </div>
   );
