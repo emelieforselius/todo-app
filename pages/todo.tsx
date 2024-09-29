ab06692ce03bb11005eeb5a99e94c995ed20c135
Emelie Forselius <emelieforselius@outlook.com>
2024-09-29 21:44:18 +0200

    Implemented add button functionality in TodoPage component

--- pages/todo.tsx
+++ pages/todo.tsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
 
+type Todo = {
+  id: number;
+  text: string;
+};
 
 const TodoPage = () => {
   const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = () => {
+    if (inputValue.trim()) {
+      const newTodo = { id: Date.now(), text: inputValue };
+      setTodos([newTodo, ...todos]);
+      setInputValue("");
+    }
+  };
 
   return (
     <div>
@@ -13,6 +26,7 @@ const TodoPage = () => {
         onChange={(e) => setInputValue(e.target.value)}
         placeholder="Add a new task..."
       />
+      <button onClick={addTodo}>Add</button>
     </div>
   );
 };
